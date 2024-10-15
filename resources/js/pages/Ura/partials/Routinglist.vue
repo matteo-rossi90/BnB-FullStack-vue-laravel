62ac8dc7fceecf97ec18935e0d46822c0aba985f
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-10-15 21:23:08 +0200

    feat: fix sidenav

--- resources/js/pages/Ura/partials/Routinglist.vue
+++ resources/js/pages/Ura/partials/Routinglist.vue
@@ -6,19 +6,19 @@ export default{
 
 <template>
     <div class="container-fluid">
-        <ul class="mt-3 d-flex flex-column justify-content-center px-4">
-            <li class="py-3">
+        <ul class="mt-3 d-flex flex-column justify-content-center">
+            <li class="py-3 d-flex align-items-center">
                 <router-link class="link" :to="{ name: 'apartments' }"
                 >
                 <i class="fa-solid fa-list pe-2"></i>
-                <span class="d-none d-lg-inline">I miei appartamenti</span></router-link
+                <span class="d-none d-xl-inline d-lg-inline">I miei appartamenti</span></router-link
                 >
             </li>
-            <li>
+            <li class="py-3 d-flex align-items-center">
                 <router-link class="link" :to="{ name: 'createApartment' }"
                 >
                 <i class="fa-solid fa-circle-plus pe-2"></i>
-                <span class="d-none d-lg-inline">I miei appartamenti</span></router-link
+                <span class="d-none d-xl-inline d-lg-inline">Nuovo appartamento</span></router-link
                 >
             </li>
         </ul>
@@ -27,11 +27,6 @@ export default{
 </template>
 
 <style lang="scss" scoped>
-
-    ul{
-        padding-left: 0;
-        // font-size: 18px;
-    }
 
 
 
