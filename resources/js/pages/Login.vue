26b30c221e63524497accd891cd4c448d65e10bf
ludoff <ludovicofiore25@gmail.com>
2024-10-15 17:40:08 +0200

    feat: login page with controls e layout fixes

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -8,7 +8,10 @@ export default {
         email: "",
         password: "",
       },
-      errors: {},
+      errors: {
+        email: '',
+        password: ''
+      },
     };
   },
   methods: {
@@ -23,25 +26,88 @@ export default {
           this.errors = err.response.data.errors;
         });
     },
+
+    validateEmail() {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+
+        if (!this.user.email) {
+            this.errors.email = "L'email è obbligatoria.";
+        } else if (!emailRegex.test(this.user.email)) {
+            this.errors.email = 'Inserisci una email valida.';
+        } else {
+            this.errors.email = '';
+        }
+    },
+
+    validatePassword() {
+        if (!this.user.password) {
+            this.errors.password = 'La password è obbligatoria.';
+        } else if (this.user.password.length < 8) {
+            this.errors.password = 'La password deve essere lunga almeno 8 caratteri.';
+        } else {
+            this.errors.password = '';
+        }
+    },
+
+    submitLogin(){
+        this.validateEmail();
+        this.validatePassword();
+
+        if (Object.values(this.errors).every(error => error === '')) {
+             this.submit()
+        }
+    }
   },
 };
 </script>
+
+
 <template>
-  <h2>Login</h2>
-  <form @submit.prevent="submit">
-    <!-- -- -->
-    <label for="email">email</label>
-    <input type="email" id="email" name="email" v-model="user.email" />
-    <label for="password">password</label>
-    <input
-      type="password"
-      id="password"
-      name="password"
-      v-model="user.password"
-    />
-    <button type="submit">submit</button>
-  </form>
+
+    <div class="container">
+        <div class="row justify-content-center">
+
+            <div class="col-8 mt-5">
+
+                <h2>Effettua il login</h2>
+                <form class="mx-auto" @submit.prevent="submitLogin">
+                  <div class="input-container">
+                      <label class="form-label" for="email">Email</label>
+                      <input class="form-control" type="email" id="email" name="email" v-model="user.email" />
+                      <small v-if="errors.email" class="error-message">{{ errors.email }}</small>
+                  </div>
+
+                  <div class="input-container">
+                      <label class="form-label" for="password">Password</label>
+                      <input
+                        class="form-control"
+                        type="password"
+                        id="password"
+                        name="password"
+                        v-model="user.password"
+                      />
+                      <small v-if="errors.password" class="error-message">{{ errors.password }}</small>
+                  </div>
+                  <button class="btn btn-primary" type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
+
+
 <style lang='scss' scoped>
 // @use 'path' as *;
+.input-container {
+    padding: 10px 0;
+
+    .error-message {
+        color: red;
+        font-size: 0.9em;
+    }
+}
+
+label {
+    display: block;
+}
 </style>
--- resources/js/pages/Register.vue
+++ resources/js/pages/Register.vue
@@ -113,8 +113,6 @@ export default {
 
         if (Object.values(this.errors).every(error => error === '')) {
              this.submit()
-        } else {
-            alert('Si prega di correggere gli errori.');
         }
 
         // this.submit();
@@ -127,63 +125,66 @@ export default {
 
 <template>
     <div class="container">
-        <div class="col-8">
-            <h2>Compila il form per registrarti</h2>
-            <!-- <ul v-if="errors">
-                <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
-            </ul> -->
-            <form @submit.prevent="submitForm">
+        <div class="row justify-content-center">
 
-                <div class="input-container">
-                    <label class="form-label" for="name">Nome</label>
-                    <input class="form-control" type="text" id="name" name="name" v-model="user.name" />
-                    <small v-if="errors.name" class="error-message">{{ errors.name }}</small>
-                </div>
+            <div class="col-8 mt-5">
+                <h2>Compila il form per registrarti</h2>
+                <!-- <ul v-if="errors">
+                    <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+                </ul> -->
+                <form @submit.prevent="submitForm">
 
-                <!-- <div class="input-container">
-                    <label class="form-label" for="surname">Cognome</label>
-                    <input class="form-control" type="text" id="surname" name="surname" v-model="user.surname" />
-                    <small v-if="errors.surname" class="error-message">{{ errors.surname }}</small>
-                </div>
+                    <div class="input-container">
+                        <label class="form-label" for="name">Nome</label>
+                        <input class="form-control" type="text" id="name" name="name" v-model="user.name" />
+                        <small v-if="errors.name" class="error-message">{{ errors.name }}</small>
+                    </div>
 
-                <div class="input-container">
-                    <label class="form-label" for="date_of_birth">Data di nascita</label>
-                    <input class="form-control" type="date" id="date_of_birth" name="date_of_birth" v-model="user.date_of_birth" />
-                    <small v-if="errors.date_of_birth" class="error-message">{{ errors.date_of_birth }}</small>
-                </div> -->
+                    <!-- <div class="input-container">
+                        <label class="form-label" for="surname">Cognome</label>
+                        <input class="form-control" type="text" id="surname" name="surname" v-model="user.surname" />
+                        <small v-if="errors.surname" class="error-message">{{ errors.surname }}</small>
+                    </div>
 
-                <div class="input-container">
-                    <label class="form-label" for="email">Email</label>
-                    <input class="form-control" type="email" id="email" name="email" v-model="user.email" />
-                    <small v-if="errors.email" class="error-message">{{ errors.email }}</small>
-                </div>
+                    <div class="input-container">
+                        <label class="form-label" for="date_of_birth">Data di nascita</label>
+                        <input class="form-control" type="date" id="date_of_birth" name="date_of_birth" v-model="user.date_of_birth" />
+                        <small v-if="errors.date_of_birth" class="error-message">{{ errors.date_of_birth }}</small>
+                    </div> -->
 
-                <div class="input-container">
-                    <label class="form-label" for="password">Password</label>
-                    <input
-                    class="form-control"
-                    type="password"
-                    id="password"
-                    name="password"
-                    v-model="user.password"
-                    />
-                    <small v-if="errors.password" class="error-message">{{ errors.password }}</small>
-                </div>
+                    <div class="input-container">
+                        <label class="form-label" for="email">Email</label>
+                        <input class="form-control" type="email" id="email" name="email" v-model="user.email" />
+                        <small v-if="errors.email" class="error-message">{{ errors.email }}</small>
+                    </div>
 
-                <div class="input-container">
-                    <label class="form-label" for="password_confirmation">Conferma la password</label>
-                    <input
-                    class="form-control"
-                    type="password"
-                    id="password_confirmation"
-                    name="password_confirmation"
-                    v-model="user.password_confirmation"
-                    />
-                    <small v-if="errors.password_confirmation" class="error-message">{{ errors.password_confirmation }}</small>
-                </div>
+                    <div class="input-container">
+                        <label class="form-label" for="password">Password</label>
+                        <input
+                        class="form-control"
+                        type="password"
+                        id="password"
+                        name="password"
+                        v-model="user.password"
+                        />
+                        <small v-if="errors.password" class="error-message">{{ errors.password }}</small>
+                    </div>
 
-                <button type="submit" class="btn btn-primary">Registrati</button>
-            </form>
+                    <div class="input-container">
+                        <label class="form-label" for="password_confirmation">Conferma la password</label>
+                        <input
+                        class="form-control"
+                        type="password"
+                        id="password_confirmation"
+                        name="password_confirmation"
+                        v-model="user.password_confirmation"
+                        />
+                        <small v-if="errors.password_confirmation" class="error-message">{{ errors.password_confirmation }}</small>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Registrati</button>
+                </form>
+            </div>
         </div>
 
     </div>
