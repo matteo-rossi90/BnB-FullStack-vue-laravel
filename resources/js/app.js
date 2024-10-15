48fb537d2a179db4ddc189b9495f4d4f8bfdd751
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-10-15 09:45:57 +0200

    install bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap';
 import App from './App.vue'
 import '../scss/_general.scss'
 import { createApp } from 'vue';
-import router from './router/index'
+import router from './router/index';
 
 const app = createApp(App)
 app.use(router)
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,15 +11,23 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.css'
+        integrity='sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ=='
+        crossorigin='anonymous' />
 
 
 </head>
 
 <body>
-    <div id="app"></div>
+    <div id="app">
+
+    </div>
     <p>Matteo</p>
     <p>Ludo</p>
-    <h1>PD (Vincenzo) test branch</h1>
+    <div class="container">
+
+        <h1 class="p-5">PD (Vincenzo) test branch</h1>
+    </div>
     <p>Ale</p>
 
 </body>
