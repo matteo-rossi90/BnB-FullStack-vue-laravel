81d68f54e77517b7ca83788f39a17af8bbfc4e38
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-10-15 11:55:25 +0200

    fix: url unknow

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('/{any}', function () {
-//     return view('welcome');
-// })->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('welcome');
+})->where('any', '.*');
