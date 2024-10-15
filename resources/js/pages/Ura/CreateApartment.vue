0b8dde3e0acaaa258ea06e41b5f6d3201caa2869
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-10-15 12:54:23 +0200

    feat: add input create

--- resources/js/pages/Ura/CreateApartment.vue
+++ resources/js/pages/Ura/CreateApartment.vue
@@ -1,14 +1,40 @@
 <script>
+
 export default {
   name: "CreateApartment",
   data() {
-    return {};
+    return {
+        // apartment:{
+        //     title:""
+        // },
+        errors: {},
+    };
   },
-  methods: {},
-};
+  methods: {
+    // submit() {
+    //   axios
+    //     .post("api/user/utente/dashboard", this.apartment)
+    //     .then(() => {
+    //       this.$router.push({ name: "dashboard" });
+    //     })
+    //     .catch((err) => {
+    //       this.errors = err.response.data.errors;
+    //     });
+  //}
+},
+}
+
 </script>
 <template>
-  <h1>apartment</h1>
+  <div class="container">
+    <h1>Inserisci il tuo appartamento</h1>
+        <form @submit.prevent="submit">
+            <label for="title">Titolo</label>
+            <input type="text" id="title" name="title"  />
+
+        </form>
+  </div>
+
 </template>
 <style lang='scss' scoped>
 // @use 'path' as *;
