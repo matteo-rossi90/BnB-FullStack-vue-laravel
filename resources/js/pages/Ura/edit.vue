dfe1df03f0bb998fd3f476d212021346cd6df16a
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-10-16 14:50:52 +0200

    fix: layout create apartments

---
+++ resources/js/pages/Ura/edit.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+
+    }
+</script>
+
+<template>
+    <div class="container-fluid">
+        <form action="">
+
+        </form>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
