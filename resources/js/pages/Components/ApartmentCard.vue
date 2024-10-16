1c14cf1fc4d062df15d283a0df839f99598fa253
ubska <alessandraubska@gmail.com>
2024-10-16 14:03:29 +0200

    feat: create static card

---
+++ resources/js/pages/Components/ApartmentCard.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'ApartmentCard'
+};
+</script>
+
+
+<template>
+    <div class="card shadow-sm border-0 rounded">
+        <img src="https://via.placeholder.com/150" class="card-img-top" alt="Appart-Img">
+        <div class="card-body">
+            <h5 class="card-title">Appartamento 1</h5>
+            <p class="card-text">Indirizzo: Via Roma, 123</p>
+            <p class="card-text">Descrizione: Appartamento con 2 camere da letto e 1 bagno.</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+</style>
