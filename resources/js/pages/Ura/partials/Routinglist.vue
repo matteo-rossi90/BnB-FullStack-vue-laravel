a473f505390e8e3be01848be809ffc9cc805b5f9
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-10-16 10:09:17 +0200

    fix responsive

--- resources/js/pages/Ura/partials/Routinglist.vue
+++ resources/js/pages/Ura/partials/Routinglist.vue
@@ -5,19 +5,19 @@ export default{
 </script>
 
 <template>
-    <div class="container-fluid">
-        <ul class="mt-3 d-flex flex-column justify-content-center">
+    <div class="mt-2 container-fluid">
+        <ul class="d-flex flex-column justify-content-center">
             <li class="py-3 d-flex align-items-center">
-                <router-link class="link" :to="{ name: 'apartments' }"
+                <router-link class="link d-flex  align-items-center gap-2" :to="{ name: 'apartments' }"
                 >
-                <i class="fa-solid fa-list pe-2"></i>
+                <i class="fa-solid fa-list"></i>
                 <span class="d-none d-xl-inline d-lg-inline">I miei appartamenti</span></router-link
                 >
             </li>
             <li class="py-3 d-flex align-items-center">
-                <router-link class="link" :to="{ name: 'createApartment' }"
+                <router-link class="link d-flex align-items-center gap-2" :to="{ name: 'createApartment' }"
                 >
-                <i class="fa-solid fa-circle-plus pe-2"></i>
+                <i class="fa-solid fa-circle-plus"></i>
                 <span class="d-none d-xl-inline d-lg-inline">Nuovo appartamento</span></router-link
                 >
             </li>
@@ -27,7 +27,6 @@ export default{
 </template>
 
 <style lang="scss" scoped>
-
-
+@use '../../../../scss/dashboard' as *;
 
 </style>
