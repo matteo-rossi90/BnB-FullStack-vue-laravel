7bea8bd19d6e55982e42182ac982bddec5b615e4
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-10-17 20:36:48 +0200

    feat: header with filter

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,7 +1,7 @@
 <script>
 import axios from "axios";
 import { store } from "../store/store";
-import { checkAdress } from "../store/store";
+
 // importo il componente ApartmentCard
 import ApartmentCard from "./components/ApartmentCard.vue";
 export default {
@@ -12,40 +12,9 @@ export default {
   data() {
     return {
       apartments: store.allApartments,
-      searchQuery: "",
-      address: [],
-      debounceTimeout: null,
     };
   },
-  methods: {
-    debouncedSearch() {
-      // Cancella il timeout precedente (se esiste)
-      clearTimeout(this.debounceTimeout);
-
-      // Imposta un nuovo timeout
-      this.debounceTimeout = setTimeout(() => {
-        this.performSearch(); // Esegui la chiamata dopo un ritardo di 300ms
-      }, 300); // Cambia questo valore per regolare il ritardo
-    },
-    performSearch() {
-      if (!this.searchQuery.trim()) {
-        return; // Non eseguire la ricerca se la query è vuota
-      }
-      let urlRequest = checkAdress(this.searchQuery);
-
-      axios
-        .get("http://127.0.0.1:8000/proxy-tomtom", {
-          params: { url: urlRequest },
-        })
-        .then((response) => {
-          this.address = response.data.results;
-          console.log(this.address[0].address);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-  },
+  methods: {},
   mounted() {
     // Chiamata per gli appartamenti pubblici
     axios
@@ -78,48 +47,17 @@ export default {
     //     console.log(err);
     //   });
   },
-  computed: {
-    suggestAdress() {
-      return this.address;
-    },
-  },
+  computed: {},
 };
 </script>
 <template>
   <!-- <h1>Home dell'utente senza login</h1> -->
-  <!-- Barra di ricerca  -->
-  <div class="container mt-4">
+
+  <!-- <div class="container mt-4">
     <div class="row justify-content-center">
-      <div class="col-md-6">
-        <div class="input-group stylish-input-group">
-          <div class="contInput">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Cerca appartamenti per indirizzo..."
-              v-model="searchQuery"
-              @input="debouncedSearch"
-            />
-            <div class="suggest" v-if="searchQuery">
-              <ul>
-                <li v-for="(addressObj, index) in suggestAdress" :key="index">
-                  {{ addressObj.address.streetName }}, n°
-                  {{
-                    addressObj.address.streetNumber
-                      ? addressObj.address.streetNumber
-                      : index
-                  }},
-                  {{ addressObj.address.municipality }}
-                  {{ addressObj.address.postalCode }}
-                  {{ addressObj.address.neighbourhood }}
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div class="col-md-6"></div>
     </div>
-  </div>
+  </div> -->
 
   <!-- card -->
   <div class="container">
@@ -139,31 +77,5 @@ export default {
 .card {
   max-width: 100%;
   margin: 10px auto;
-}
-.contInput {
-  width: 70%;
-  padding: 0.5rem;
-  border-radius: 20px;
-  border: 1px solid black;
-  position: relative;
-}
-.suggest {
-  position: absolute;
-  width: 100%;
-  top: 100%;
-  background-color: rgb(237, 237, 237);
-  border-radius: 20px;
-  font-size: 0.75rem;
-  z-index: 10;
-
-  li {
-    padding: 0.3rem 0.5rem;
-    border-radius: 20px;
-    margin-bottom: 0.1rem;
-    &:hover {
-      background-color: rgb(217, 217, 217);
-      cursor: pointer;
-    }
-  }
 }
 </style>
