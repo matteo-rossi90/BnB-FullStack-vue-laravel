38b0cd315219dd4fb571c06746e175d2267be7a6
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-10-22 19:17:29 +0200

    feat: add function to click on thumbnail to show the text message [need to fix images]

--- resources/js/pages/Ura/Messages.vue
+++ resources/js/pages/Ura/Messages.vue
@@ -10,13 +10,40 @@ export default {
   data() {
     return {
       messages: [],
+      selectedMessage: null,
+      selectedImage: null,
+      images: [
+        "https://picsum.photos/seed/profile1/80/80",
+        "https://picsum.photos/seed/profile2/80/80",
+        "https://picsum.photos/seed/profile3/80/80",
+        "https://picsum.photos/seed/profile4/80/80",
+        "https://picsum.photos/seed/profile5/80/80",
+        "https://picsum.photos/seed/profile6/80/80",
+        "https://picsum.photos/seed/profile7/80/80",
+        "https://picsum.photos/seed/profile8/80/80",
+        "https://picsum.photos/seed/profile9/80/80",
+        "https://picsum.photos/seed/profile10/80/80",
+      ],
     };
+  },
+  methods:{
+    formatDate(dateStr){
+      const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+      const date = new Date(dateStr);
+      return date.toLocaleDateString('it-IT', options);
+    },
+    getImage(index) {
+      return this.images[index % this.images.length];
+    },
+    selectMessage(message) {
+      this.selectedMessage = message;
+      this.selectedImage = this.getImage(index)
+    },
   },
   mounted() {
     axios
       .get("api/user/utente/messaggi")
       .then((res) => {
-
         this.messages = res.data;
       })
       .catch((err) => {
@@ -29,42 +56,52 @@ export default {
 <template>
   <div class="wrapper d-flex">
     <aside>
-        <div class="container">
-            <div class="d-flex justify-content-center my-4">
-                <h4>Tutti i messaggi</h4>
+      <div class="container">
+
+        <div class="d-flex justify-content-center my-4">
+          <h4>Tutti i messaggi</h4>
+        </div>
+
+        <div class="d-flex mx-4">
+          <input type="search" class="form-control" id="box-search">
+          <button type="submit" class="search-button">
+            <i class="fa-solid fa-search"></i>
+          </button>
+        </div>
+
+        <div class="message-list mt-4">
+          <MessagesList :messages="messages" @selectMessage="selectMessage" />
+        </div>
+
+      </div>
+
+    </aside>
+
+    <div class="dashboard-box">
+      <div class="container py-4">
+
+        <div v-if="selectedMessage">
+          <h5>{{ selectedMessage.name }}</h5>
+
+          <div class="d-flex align-items-center">
+            <div class="image mx-3">
+              <img :src="selectedImage" alt="utente">
             </div>
 
-            <div class="d-flex mx-4">
-                <input type="search" class="form-control" id="box-search">
-                <button type="submit" class="search-button">
-                    <i class="fa-solid fa-search"></i>
-                </button>
+            <div class="text-box my-3">
+              <h6>{{ selectedMessage.name }} {{ selectedMessage.surname }}</h6>
+              <small>{{ formatDate(selectedMessage.created_at) }}</small>
+              <small>{{ selectedMessage.email }}</small>
+              <p>{{ selectedMessage.message }}</p>
             </div>
-            <div class="message-list mt-4">
-                <MessagesList />
-            </div>
+
+          </div>
 
         </div>
 
-    </aside>
-    <div class="dashboard-box">
-      <div class="container-fluid my-3">
-
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item d-flex align-items-center" v-for="(message, index) in messages" :key="index">
-                <div class="image mx-3">
-                    <img src="profile-circle.svg" alt="">
-                </div>
-                <div class="text-box my-3">
-                    <div class="d-flex">
-                        <h6>{{ message.name }} {{ message.surname }}</h6>
-                        <small>{{ message.created_at }}</small>
-                    </div>
-                    <small>{{ message.email }}</small>
-                    <p class="message-email">{{ message.message }}</p>
-                </div>
-            </li>
-        </ul>
+        <div v-else>
+          <p>Seleziona un messaggio dalla lista</p>
+        </div>
 
       </div>
     </div>
@@ -75,49 +112,47 @@ export default {
 @use "../../../scss/variables" as *;
 @use "../../../scss/message" as *;
 
-input{
-    border-radius: 50px;
+input {
+  border-radius: 50px;
 }
 
-#box-search{
-    position: relative;
+#box-search {
+  position: relative;
 }
 
-.search-button{
-    position: absolute;
-    right:2.5rem;
-    padding:5px;
-    background-color: transparent;
-    border: none;
+.search-button {
+  position: absolute;
+  right: 2.5rem;
+  padding: 5px;
+  background-color: transparent;
+  border: none;
 }
 
-.message-list{
-    height: calc(100vh - 300px);
-    padding:10px;
-    width: 100%;
-    overflow-y:auto;
+.message-list {
+  height: calc(100vh - 300px);
+  padding: 10px;
+  width: 100%;
+  overflow-y: auto;
 
-    //personalizzazione scrollbar per Chrome
-    ::-webkit-scrollbar {
-        width: 8px;
-    }
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
 
-    ::-webkit-scrollbar-track {
-        background: #f1f1f1;
-    }
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
 
-    ::-webkit-scrollbar-thumb {
-        background-color: #888;
-        border-radius: 10px;
-        border: 2px solid #f1f1f1;
-    }
+  ::-webkit-scrollbar-thumb {
+    background-color: #888;
+    border-radius: 10px;
+    border: 2px solid #f1f1f1;
+  }
 
-    ::-webkit-scrollbar-thumb:hover {
-        background: #555;
-    }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
 
-    //personalizzazione scrollbar per Forefox
-    scrollbar-width: thin;
-    scrollbar-color: #888 #f1f1f1;
+  scrollbar-width: thin;
+  scrollbar-color: #888 #f1f1f1;
 }
 </style>
--- resources/js/pages/Ura/partials/MessagesList.vue
+++ resources/js/pages/Ura/partials/MessagesList.vue
@@ -1,47 +1,58 @@
 <script>
-import axios from 'axios';
-export default{
-    name: 'MessageList',
-    data(){
-        return{
-            messages:[]
-        }
+export default {
+  name: 'MessagesList',
+  props: {
+    messages: {
+      type: Array,
+      required: true,
     },
-    mounted() {
-        axios
-            .get("api/user/utente/messaggi")
-            .then((res) => {
-            this.messages = res.data;
-            })
-            .catch((err) => {
-            console.log(err);
-            });
-        },
-}
+  },
+  methods: {
+    formatDate(dateStr) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      const date = new Date(dateStr);
+      return date.toLocaleDateString('it-IT', options);
+    },
+
+    getImage(index) {
+      return this.$parent.getImage(index);
+    },
+
+    selectMessage(message, index) {
+      this.$emit('selectMessage', message, index);
+    },
+
+  },
+};
 </script>
 
 <template>
 
-<div>
     <ul class="list-group list-group-flush">
-        <li class="list-group-item d-flex align-items-center" v-for="(message, index) in messages" :key="index">
-            <div class="image mx-3">
-                <img src="profile-circle.svg" alt="">
-            </div>
-            <div class="text-box my-3">
-                <div class="d-flex">
-                    <h6>{{ message.name }} {{ message.surname }}</h6>
-                    <small>{{ message.created_at }}</small>
-                </div>
-                <small>{{ message.email }}</small>
-                <p class="message-email">{{ message.message }}</p>
-            </div>
-        </li>
+
+      <li class="list-group-item d-flex align-items-center"
+          v-for="(message, index) in messages"
+          :key="index"
+          @click="selectMessage(message, index)">
+        <div class="image mx-3">
+          <img :src="getImage(index)" alt="utente">
+        </div>
+
+        <div class="text-box my-3">
+          <div class="d-flex justify-content-between">
+            <h6>{{ message.name }} {{ message.surname }}</h6>
+            <small>{{ formatDate(message.created_at) }}</small>
+          </div>
+          <small>{{ message.email }}</small>
+          <p class="message-email">{{ message.message }}</p>
+        </div>
+
+      </li>
+
     </ul>
-</div>
+
 </template>
 
 <style lang="scss">
 @use '../../../../scss/message' as*;
-
 </style>
