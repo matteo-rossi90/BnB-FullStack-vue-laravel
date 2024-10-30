0206f1fd493900a5e382c1b4d1736d3cca195e84
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-10-30 16:23:45 +0100

    fix: move number of apartments in the main title in IndexApartments

--- resources/js/pages/Ura/IndexApartment.vue
+++ resources/js/pages/Ura/IndexApartment.vue
@@ -185,9 +185,9 @@ export default {
       </router-link>
       <div class="row">
         <div class="col-lg-12 col-md-12">
-          <h2 class="my-4">I miei appartamenti</h2>
+          <h2 class="my-4">I miei appartamenti: {{ apartments.length }}</h2>
 
-          <h4 class="my-5">Appartamenti totali: {{ apartments.length }}</h4>
+          <!-- <h4 class="my-5">Appartamenti totali: {{ apartments.length }}</h4> -->
 
           <div class="table-responsive">
             <table class="table">
