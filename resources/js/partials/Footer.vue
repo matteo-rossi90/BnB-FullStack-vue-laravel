78ab746f8d970c599a1f1fb2baa9fb870f9b9786
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-11-03 11:44:02 +0100

    fix: add responsive in footer

--- resources/js/partials/Footer.vue
+++ resources/js/partials/Footer.vue
@@ -8,13 +8,13 @@ export default {
 };
 </script>
 <template>
-    <div class="container-fluid d-flex gap-3  ms_container">
+    <div class="container-fluid d-flex flex-wrap gap-3  ms_container">
 
-        <div class="align-content-center">&copy; BolBnb 2024</div>
-        <router-link class="align-content-center" to="/">Home</router-link>
-        <router-link class="align-content-center" to="/chi-siamo"> chi siamo</router-link>
-        <router-link class="align-content-center" to="/privacy-policy"> Privacy policy</router-link>
-        <router-link class="align-content-center" to="/company"> La nostra azienda</router-link>
+        <div class="copy-right">&copy; BolBnb 2024</div>
+        <router-link class="link" to="/">Home</router-link>
+        <router-link class="link" to="/chi-siamo"> chi siamo</router-link>
+        <router-link class="link" to="/privacy-policy"> Privacy policy</router-link>
+        <router-link class="link" to="/company"> La nostra azienda</router-link>
 
 
 
@@ -27,23 +27,58 @@ export default {
 .ms_container{
     // position: absolute;
     // bottom: 0;
-
+    font-size: 1rem;
     padding:20px;
-    height: 100px;
+    height: auto;
     background-color: rgb(103, 100, 100);
     color: white;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
     a{
 
        color: white;
        text-decoration: none;
-       align-items: center;
+    //    align-items: center;
        &:hover{
         text-decoration: underline;
-        color: red;
-        transform: scale(1.3);
-        transition: transform 1s cubic-bezier(0.25, 0.1, 0.25, 1);
+        // color: red;
+        // transform: scale(1.3);
+        // transition: transform 1s cubic-bezier(0.25, 0.1, 0.25, 1);
+
        }
+
     }
+
+        @media (max-width: 768px) {
+            flex-direction: column;
+            justify-content: center;
+            text-align: center;
+
+        .link {
+            font-size: 0.9rem;
+            padding: 5px 0;
+        }
+
+        .copy-right {
+            font-size: 0.9rem;
+            margin-bottom: 10px;
+        }
+    }
+
+    @media (max-width: 550px) {
+        font-size: 0.8rem;
+
+        .link {
+            font-size: 0.8rem;
+        }
+
+        .copy-right {
+            font-size: 0.8rem;
+        }
+  }
+
 
 }
 </style>
