684a64ebce7d68164d45c5be71dd0fd13982aa50
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-11-03 11:38:22 +0100

    fix: sidebar

--- resources/js/partials/Footer.vue
+++ resources/js/partials/Footer.vue
@@ -8,42 +8,49 @@ export default {
 };
 </script>
 <template>
-    <div class="container-fluid d-flex gap-3  ms_container">
-
-        <div class="align-content-center">&copy; BolBnb 2024</div>
-        <router-link class="align-content-center" to="/">Home</router-link>
-        <router-link class="align-content-center" to="/chi-siamo"> chi siamo</router-link>
-        <router-link class="align-content-center" to="/privacy-policy"> Privacy policy</router-link>
-        <router-link class="align-content-center" to="/company"> La nostra azienda</router-link>
-
-
-
-
+  <footer>
+    <div class="container-fluid d-flex gap-3 ms_container">
+      <div class="align-content-center">&copy; BolBnb 2024</div>
+      <router-link class="align-content-center" to="/">Home</router-link>
+      <router-link class="align-content-center" to="/chi-siamo">
+        chi siamo</router-link
+      >
+      <router-link class="align-content-center" to="/privacy-policy">
+        Privacy policy</router-link
+      >
+      <router-link class="align-content-center" to="/company">
+        La nostra azienda</router-link
+      >
     </div>
+  </footer>
 </template>
 <style lang='scss' scoped>
 // @use 'path' as *;
+// footer {
+//   z-index: 100;
+//   position: fixed;
+//   top: calc(100% - 6rem);
+//   left: 0;
+//   width: 100%;
+// }
+.ms_container {
+  // position: absolute;
+  // bottom: 0;
 
-.ms_container{
-    // position: absolute;
-    // bottom: 0;
-
-    padding:20px;
-    height: 100px;
-    background-color: rgb(103, 100, 100);
+  padding: 20px;
+  height: 100px;
+  background-color: rgb(103, 100, 100);
+  color: white;
+  a {
     color: white;
-    a{
-
-       color: white;
-       text-decoration: none;
-       align-items: center;
-       &:hover{
-        text-decoration: underline;
-        color: red;
-        transform: scale(1.3);
-        transition: transform 1s cubic-bezier(0.25, 0.1, 0.25, 1);
-       }
+    text-decoration: none;
+    align-items: center;
+    &:hover {
+      text-decoration: underline;
+      color: red;
+      transform: scale(1.3);
+      transition: transform 1s cubic-bezier(0.25, 0.1, 0.25, 1);
     }
-
+  }
 }
 </style>
